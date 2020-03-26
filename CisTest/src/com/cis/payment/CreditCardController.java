1ab3b5a6911b62d664d2ba69b59bf5b4b6827fa8
bhimesh15@gmail.com <bhimesh15@gmail.com>
2020-03-26 22:34:06 +0530

    3rd commit. adding PaymentControllerTest and changes in CCContoller.

---
+++ CisTest/src/com/cis/payment/CreditCardController.java
@@ -0,0 +1,9 @@
+package com.cis.payment;
+
+public class CreditCardController {
+
+	public static void main(String[] args) {
+		System.out.println("CCController is created...");
+		System.out.println("added more code in 3rd commit");
+	}
+}
---
+++ CisTest/src/com/cis/test/PaymentControllerTest.java
@@ -0,0 +1,14 @@
+package com.cis.test;
+
+public class PaymentControllerTest {
+
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Payment controller is tested...");
+		System.out.println("adding...");
+	}
+}
